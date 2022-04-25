842b2009679e7e31cda4d510c335cfc6506d6aae
voitivit <kurbanov_emil@icloud.com>
2022-04-25 19:31:34 +0300

    Lesson1

--- RunMap/RunMap/ViewController.swift
+++ RunMap/RunMap/ViewController.swift
@@ -6,14 +6,116 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var mapView: GMSMapView!
+    let coordinate = CLLocationCoordinate2D(latitude: 37.34033264974476, longitude: -122.06892632102273)
+    var marker: GMSMarker?
+    var geoCoder: CLGeocoder?
+    var route: GMSPolyline?
+    var locationManager: CLLocationManager?
+    var routePath: GMSMutablePath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureMap()
+        configureLocationManager()
     }
+    
+    private func configureLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.allowsBackgroundLocationUpdates = true
+    }
+    
+    private func configureMap() {
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15)
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        
+        mapView.delegate = self
+        
+        
+    }
+    private func addMarker() {
+        marker = GMSMarker(position: coordinate)
 
-
+        marker?.icon = GMSMarker.markerImage(with: .blue)
+        marker?.title = "Маркер"
+        marker?.snippet = "Новый маркер"
+        
+        marker?.map = mapView
+    }
+    
+    private func removeMarker() {
+        marker?.map = nil
+        marker = nil
+    }
+    
+    
+    
+    
+    @IBAction func didTapUpdateLocation(_ sender: UIButton) {
+        locationManager?.requestLocation()
+        route?.map = nil
+        route = GMSPolyline()
+        routePath = GMSMutablePath()
+        route?.map = mapView
+        
+        locationManager?.startUpdatingLocation()
+    }
+    
+    
+    @IBAction func addMarker(_ sender: UIButton) {
+        if marker == nil {
+            mapView.animate(toLocation: coordinate)
+            addMarker()
+        } else {
+            removeMarker()
+        }
+    }
+    
 }
+
+    
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print(coordinate)
+        let manulMarker = GMSMarker(position: coordinate)
+        manulMarker.map = mapView
+        
+        if geoCoder == nil {
+            geoCoder = CLGeocoder()
+        }
+        
+        geoCoder?.reverseGeocodeLocation(CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude), completionHandler: { places, error in
+            print(places?.last)
+        })
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        routePath?.add(location.coordinate)
+        route?.path = routePath
+        
+        let position = GMSCameraPosition.camera(withTarget: location.coordinate , zoom: 15)
+        mapView.animate(to: position)
+        
+        print(location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+
 
